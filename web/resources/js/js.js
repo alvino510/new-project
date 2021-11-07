7bfc8b8d1015283232f50a52c68e65fd9ef3a4f7
majestic510 <85354679+majestic510@users.noreply.github.com>
2021-11-07 14:23:27 +0700

    Add files via upload

---
+++ web/resources/js/js.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Get the button:
+mybutton = document.getElementById("active");
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
